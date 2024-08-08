f1f53d0c3e8504c95654edd8fe12e24a040f1c12
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-08-08 12:09:42 +0530

    episode 3

--- app.js
+++ app.js
@@ -1,21 +1,69 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-{/* <div id="parent">
-  <div id="child">
-    <h1 id="h1tag"></h1>
-    <h2 id="h2tag"></h2>
-  </div>
-</div> */}
 
-const item=React.createElement('div',{id:"parent"},
-  React.createElement('div',{id:"child"},
-    [
-      React.createElement('h1',{id:'h1tag'},"Manoj is Best!"),
-      React.createElement('h2',{id:'h2tag'},"Number one or only one ")
-    ]
-  )
+const jsxHead=(
+<h1 
+id="heading">
+  Ketaki is Best!
+  </h1>
 );
 
 const root=ReactDOM.createRoot(document.querySelector('#root'));
+// root.render(jsxHead);
 
-root.render(item);
+const HeadElement=()=><h1>Hello</h1>;
+
+// this is component composition
+const HeadElement1=()=>{
+  return <div id ="container">
+    <HeadElement/>
+      <h1 id="heading">Hello world!</h1>
+      <h2 className="heading">Hello Manoj!</h2>
+  </div>
+};
+
+// root.render(<HeadElement1/>);
+
+//another example
+
+// this is react element 
+const desc=(
+   <>
+  <h1>This is react element</h1>
+  <h2>Yes this is react element</h2>
+  </>
+);
+
+
+const number=100;
+const Title=()=>{
+  return <>
+  {desc}
+    <h1>CrafNeuron</h1>
+    <h2>Powered by Manoj</h2>
+  </>
+};
+const Main=()=>{
+  return <>
+
+    <h1>{number+100*100}</h1>
+    <Title/>
+    <h2>Protected by Happiness!</h2>
+  </>
+};
+const anotherRoot=ReactDOM.createRoot(document.querySelector('#container'));
+anotherRoot.render(<Main/>);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
